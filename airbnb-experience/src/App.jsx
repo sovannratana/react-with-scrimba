37b6883e1c367b88ee1b712939361e52824e758d
Sovannratana OUK <sovannratana18@gmail.com>
2023-02-04 10:42:20 +0700

    finished the project

--- airbnb-experience/src/App.jsx
+++ airbnb-experience/src/App.jsx
@@ -2,42 +2,29 @@ import "./App.css";
 import Card from "./components/Card";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import data from "./data";
 
 function App() {
-  const data = [
-    {
-      id: 1,
-      img: "card1.svg",
-      rating: "5.0",
-      reviewCount: 6,
-      country: "USA",
-      title: "Life lessons with Katie Zaferes",
-      price: 136,
-    },
-    {
-      id: 2,
-      img: "card2.svg",
-      rating: "5.0",
-      reviewCount: 30,
-      country: "USA",
-      title: "Learn wedding photography",
-      price: 125,
-    },
-    {
-      id: 3,
-      img: "card3.svg",
-      rating: "4.8",
-      reviewCount: 2,
-      country: "USA",
-      title: "Group Mountain Biking",
-      price: 50,
-    },
-  ];
+  const cards = data.map((item) => {
+    return (
+      <Card
+        key={item.id}
+        openSpots={item.openSpots}
+        img={item.img}
+        rating={item.rating}
+        reviewCount={item.reviewCount}
+        country={item.country}
+        title={item.title}
+        price={item.price}
+        location={item.location}
+      />
+    );
+  });
   return (
     <div className="app bg-white rounded-lg">
       <Navbar />
       <Hero />
-      <Card data={data} />
+      <section className="cards flex ml-8 pb-6 space-x-5">{cards}</section>
     </div>
   );
 }
--- airbnb-experience/src/components/Card.jsx
+++ airbnb-experience/src/components/Card.jsx
@@ -1,25 +1,32 @@
 const Card = (props) => {
-  const datas = props.data;
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
   return (
-    <div className="cards flex ml-8 pb-6 space-x-5">
-      {datas.map((data) => (
-        <div className="mt-12 max-w-[176px]" key={data.id}>
-          <img src={`src/assets/${data.img}`} alt="" />
-          <div className="rating flex mt-1 text-[#918E9B]">
-            <img src="src/assets/star.svg" alt="" className="pr-1" />
-            <span className="text-xs text-black pr-1">{data.rating}</span>
-            <span className="text-xs">({data.reviewCount})</span>
-            <span className="text-xs">•{data.country}</span>
-          </div>
-          <div className="detail text-[12px] ">
-            <p className="">{data.title}</p>
-          </div>
-          <div className="Price flex text-[12px]">
-            <p className="pr-1 font-bold">From ${data.price}</p>
-            <p>/ person</p>
-          </div>
+    <div className="mt-12 max-w-[176px]" key={props.id}>
+      {badgeText && (
+        <div className="text-[10.24px] absolute bg-white px-2 py-1 mt-1 ml-1 rounded-sm">
+          {badgeText}
         </div>
-      ))}
+      )}
+      <img src={`src/assets/${props.img}`} alt="" />
+      <div className="rating flex mt-1 text-[#918E9B]">
+        <img src="src/assets/star.svg" alt="" className="pr-1" />
+        <span className="text-xs text-black pr-1">{props.rating}</span>
+        <span className="text-xs">({props.reviewCount})</span>
+        <span className="text-xs">•{props.country}</span>
+      </div>
+      <div className="detail text-[12px] ">
+        <p className="">{props.title}</p>
+      </div>
+      <div className="Price flex text-[12px]">
+        <p className="pr-1 font-bold">From ${props.price}</p>
+        <p>/ person</p>
+      </div>
     </div>
   );
 };
